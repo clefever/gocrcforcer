c0209385f518900ea58a3ac9df55dde3e4ad360e
Chris Lefever <linuxcl4@gmail.com>
2018-11-14 17:49:53 -0500

    Use log.Fatalln() which prints and exits with error code

--- cmd/gcf/main.go
+++ cmd/gcf/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"strconv"
 
@@ -11,32 +11,27 @@ import (
 func main() {
 	// Handle arguments
 	if len(os.Args) != 4 {
-		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "FileName ByteOffset NewCrc32Value")
-		os.Exit(1)
+		log.Fatalln("Usage:", os.Args[0], "FileName ByteOffset NewCrc32Value")
 	}
 
 	// Parse and check file offset argument
 	offset, err := strconv.ParseInt(os.Args[2], 10, 64)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error: Invalid byte offset")
-		os.Exit(1)
+		log.Fatalln("Error: Invalid byte offset")
 	}
 
 	// Parse and check new CRC argument
 	if len(os.Args[3]) != 8 {
-		fmt.Fprintln(os.Stderr, "Error: Invalid new CRC-32 value")
-		os.Exit(1)
+		log.Fatalln("Error: Invalid new CRC-32 value")
 	}
 	newcrc, err := strconv.ParseUint(os.Args[3], 16, 32)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error: Invalid new CRC-32 value")
-		os.Exit(1)
+		log.Fatalln("Error: Invalid new CRC-32 value")
 	}
 
 	// Process the file
 	err = gocrcforcer.ModifyFileCrc32(os.Args[1], offset, uint32(newcrc), true)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
+		log.Fatalln(err.Error())
 	}
 }
